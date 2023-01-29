631f48b6681ee9bbb7a82d2076d7ccae99569642
natalyq1 <onatylili@gmail.com>
2023-01-28 21:22:11 -0500

    nuevaTransferencia interface set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: "<h1>Hola desde nueva transferencia</h1>",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ByteBank';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { NuevaTransferenciaComponent } from './NuevaTransferencia/nueva-transferencia.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevaTransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
